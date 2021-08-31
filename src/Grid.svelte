40c60373bda71f9bf4cf3ff1fbd9fbc5b9ef610a
Sam Firminger <samfirminger@gmail.com>
2021-08-31 19:54:40 +0100

    Added grid

---
+++ src/Grid.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let gridSize;
+    export let snakeHeadPosX;
+    export let snakeHeadPosY;
+</script>
+
+<style>
+    .grid {
+        width: 50%;
+        margin: 0 auto;
+    }
+
+    .row {
+        display: flex;
+    }
+
+    .cell {
+        background-color: #F0D9FF;
+        height: 50px;
+        width: 50px;
+        border: 1px solid black;
+    }
+
+    .snake {
+        background-color: #BFA2DB;
+    }
+</style>
+
+<div class="grid">
+    {snakeHeadPosX} {snakeHeadPosY}
+    {#each Array(gridSize) as _, i}
+        <div class="row">
+            {#each Array(gridSize) as _, j}
+                <div class="cell {snakeHeadPosX === j && snakeHeadPosY === i ? 'snake' : ''}"></div>
+            {/each}
+        </div>
+    {/each}
+</div>
